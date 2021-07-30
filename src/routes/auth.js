bd25030f0b2f8e32562370c08a7dbd10f7870be2
alex-146 <deynekos1488@gmail.com>
2021-07-31 02:01:00 +0300

    auth api

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,19 +1,45 @@
-const axios = require("axios");
-const router = require("express").Router();
+const express = require("express");
+const router = express.Router();
 const VkApi = require("../services/VkApi");
+const EventEmitter = require("events").EventEmitter;
+
+const emitter = new EventEmitter();
 
 router.get("/", (req, res) => {
   return res.json(VkApi.getAuth());
 });
 
-router.get("/:id", (req, res) => {
-  const { code } = req.query;
-  const { id } = req.params;
-  console.log(code, id);
+router.get("/:id", async (req, res) => {
+  try {
+    const { code } = req.query;
+    const { id } = req.params;
+    if (code && id) {
+      const { data } = await VkApi.getAccessToken(code, id);
+      const { access_token } = data;
+      emitter.emit(`TOKEN_RECEIVED_${id}`, access_token);   
+      res.json({ ok: true });
+    }
+  }
+  catch(error) {
+    return res.status(500).json({ ok: false, message: error.message });
+  }
 });
 
-router.post("/", (req, res) => {
-  //todo: long polling
+router.post("/", express.urlencoded({ extended: false }), (req, res) => {
+  try {
+    const id = req.body.id;
+    if (id) {
+      emitter.once(`TOKEN_RECEIVED_${id}`, (access_token) => {
+        return res.json({ access_token });
+      });
+    }
+    else {
+      return res.status(400).json({ ok: false, message: "provide id" });
+    }
+  }
+  catch(error) {
+    return res.status(500).json({ ok: false, message: error.message });
+  }
 });
 
 module.exports = router;
